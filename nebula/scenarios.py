02111ebfe330576a82b31bc7cb5f43b805615755
Alejandro.A.S <jandrosambasil@gmail.com>
2025-03-13 17:37:13 +0100

    fix additional participants datasets

--- nebula/scenarios.py
+++ nebula/scenarios.py
@@ -579,7 +579,7 @@ class ScenarioManagement:
         if dataset_name == "MNIST":
             dataset = MNISTDataset(
                 num_classes=10,
-                partitions_number=self.n_nodes,
+                partitions_number=self.n_nodes+additional_nodes,
                 iid=self.scenario.iid,
                 partition=self.scenario.partition_selection,
                 partition_parameter=self.scenario.partition_parameter,
@@ -589,7 +589,7 @@ class ScenarioManagement:
         elif dataset_name == "FashionMNIST":
             dataset = FashionMNISTDataset(
                 num_classes=10,
-                partitions_number=self.n_nodes,
+                partitions_number=self.n_nodes+additional_nodes,
                 iid=self.scenario.iid,
                 partition=self.scenario.partition_selection,
                 partition_parameter=self.scenario.partition_parameter,
@@ -599,7 +599,7 @@ class ScenarioManagement:
         elif dataset_name == "EMNIST":
             dataset = EMNISTDataset(
                 num_classes=10,
-                partitions_number=self.n_nodes,
+                partitions_number=self.n_nodes+additional_nodes,
                 iid=self.scenario.iid,
                 partition=self.scenario.partition_selection,
                 partition_parameter=self.scenario.partition_parameter,
@@ -609,7 +609,7 @@ class ScenarioManagement:
         elif dataset_name == "CIFAR10":
             dataset = CIFAR10Dataset(
                 num_classes=10,
-                partitions_number=self.n_nodes,
+                partitions_number=self.n_nodes+additional_nodes,
                 iid=self.scenario.iid,
                 partition=self.scenario.partition_selection,
                 partition_parameter=self.scenario.partition_parameter,
@@ -619,7 +619,7 @@ class ScenarioManagement:
         elif dataset_name == "CIFAR100":
             dataset = CIFAR100Dataset(
                 num_classes=100,
-                partitions_number=self.n_nodes,
+                partitions_number=self.n_nodes+additional_nodes,
                 iid=self.scenario.iid,
                 partition=self.scenario.partition_selection,
                 partition_parameter=self.scenario.partition_parameter,
